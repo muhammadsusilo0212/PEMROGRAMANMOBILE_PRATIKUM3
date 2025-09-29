806ee1d74cae9ba3b7a31b7d304d4d7155a5fcf8
muhammadsusilo0212 <muhammadsusilo444@gmail.com>
2025-09-29 13:15:27 +0700

    commit first

--- src/id/ac/polbeng/depandi/test_kelas/Greeter.kt
+++
@@ -1,7 +0,0 @@
-package id.ac.polbeng.depandi.test_kelas
-
-class Greeter {
-    fun greet() {
-        println("Hello object world!")
-    }
-}
--- src/id/ac/polbeng/depandi/test_kelas/GreeterTest.kt
+++
@@ -1,6 +0,0 @@
-package id.ac.polbeng.depandi.test_kelas
-
-fun main() {
-    val greeter = Greeter()
-    greeter.greet()
-}
