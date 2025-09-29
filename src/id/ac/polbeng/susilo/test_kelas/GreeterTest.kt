17b299e10065b17fb3d94cf906fe05cb8e226263
muhammadsusilo0212 <muhammadsusilo444@gmail.com>
2025-09-29 13:15:52 +0700

    commit first

---
+++ src/id/ac/polbeng/susilo/test_kelas/GreeterTest.kt
@@ -0,0 +1,21 @@
+/*package id.ac.polbeng.depandi.test_kelas
+
+fun main() {
+    val greeter = Greeter()
+    greeter.greet()
+}*/
+//batassss
+
+
+
+package id.ac.polbeng.susilo.test_kelas
+
+fun main() {
+    val greeter = Greeter()
+    greeter.greet()
+    greeter.text = "Hi"
+    greeter.greet("Anton")
+    greeter.greet("Budi")
+    greeter.text = "Hello programmer"
+    println(greeter.with_ret_val("Dono"))
+}
